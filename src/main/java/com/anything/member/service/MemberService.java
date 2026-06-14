384c7bbb538f9e93278ce2e662dbe4b739352777
JaeHyun-Ban <wogus0008@gmail.com>
2026-06-15 01:02:41 +0900

    [upt] 사용자 정보가져오기, 로그인 시 JWT 토큰 반환

--- src/main/java/com/anything/member/service/MemberService.java
+++ src/main/java/com/anything/member/service/MemberService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.anything.global.security.JwtUtil;
+import com.anything.member.dto.LoginResponse;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +18,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    private  final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtUtil jwtUtil;
 
     @Transactional
     public MemberResponse signup(SignupRequest request) {
@@ -36,5 +40,24 @@ public class MemberService {
                 .build();
 
         return new MemberResponse(memberRepository.save(member));
+    }
+
+    public MemberResponse getMyInfo(String loginId) {
+        Member member = memberRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다"));
+        return new MemberResponse(member);
+    }
+
+    // 로그인 메서드
+    public LoginResponse login(LoginRequest request) {
+        Member member = memberRepository.findByLoginId(request.getLoginId())
+                .orElseThrow(() -> new IllegalArgumentException("아이디 또는 비밀번호가 올바르지 않습니다."));
+
+        if (!passwordEncoder.matches(request.getPassword(), member.getPassword())) {
+            throw new IllegalArgumentException("아이디 또는 비밀번호가 올바르지 않습니다.");
+        }
+
+        String token = jwtUtil.generateToken(member.getLoginId());
+        return new LoginResponse(token, new MemberResponse(member));
     }
 }
